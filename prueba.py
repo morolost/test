897d101829401958631b2d9b7fcd0f4286cb295e
morolost <moronacho57@gmail.com>
2024-08-25 17:09:33 -0300

    subo proyecto

---
+++ prueba.py
@@ -0,0 +1,26 @@
+import os
+
+cadena = "jorge"
+
+booleano = True
+
+numero = 12
+
+flotante = 12.5
+
+a = 0
+
+    
+
+while a < 12:
+    a += 1
+    print(a)
+
+if numero == a:
+    b = 3
+    print(b)
+else:
+    b = 2
+    print(b)
+
+os.system("pause")
