e1b5ba2a402cdad4fad068f2f57794170fe79ad5
morolost <moronacho57@gmail.com>
2024-09-05 19:05:34 -0300

    v1.0

---
+++ PRUEBA ROCA.py
@@ -0,0 +1,32 @@
+import os
+os.system ("cls")
+numeros = []
+
+for i in range(3):
+    numero = int(input(f"Ingrese el número {i+1}: "))
+    if numero <= 0:
+        print("no es mayor a 0")
+    numeros.append(numero) 
+print(numeros)
+
+os.system("pause")
+
+for numero in numeros[:]:  
+    if numero <= 0:
+        numeros.remove(numero) 
+        print(f"El número {numero} ha sido eliminado por ser menor o igual a 0.")
+        
+numeros.sort
+
+intervalo_completo = []
+
+for i in range(len(numeros) - 1):
+    intervalo_completo.append(numeros[i])
+    
+    for j in range(int(numeros[i]) + 1, int(numeros[i + 1])):
+        intervalo_completo.append(j)
+
+if numeros:
+    intervalo_completo.append(numeros[-1])
+
+print(intervalo_completo)
